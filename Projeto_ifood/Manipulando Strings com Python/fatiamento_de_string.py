70601d9a344d4d8dce8fa5726c87c5734eaf99ec
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2023-07-16 16:14:23 -0300

    16-07
    Strings em Python

---
+++ Projeto_ifood/Manipulando Strings com Python/fatiamento_de_string.py
@@ -0,0 +1,9 @@
+nome = 'Matheus Felipe Braga de Souza'
+
+print(nome[0]) # Exibe a letra da casa citada
+print(nome[:9]) # Exibe até a casa da letra citada
+print(nome[10:]) # Exibe a letra desde a casa citada
+print(nome[10:16]) # Exibe as letras entre as casas citadas
+print(nome[10:16:2]) # Exibe as letras entre as casas citadas, pulando de 2 em 2 passo citado
+print(nome[:]) # Exibe a string inteira
+print(nome[::-1]) # Exibe de trás para frente
---
+++ Projeto_ifood/Manipulando Strings com Python/interpolacao_de_variaveis.py
@@ -0,0 +1,15 @@
+nome = 'Matheus'
+idade = 34
+profissao = 'coordenador'
+linguagem = 'Python'
+
+print(f'Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou matriculado no curso de {linguagem}.')
+
+print()
+#------------------------------------------------
+PI = 3.14159
+
+print(f'O valor de pi é {PI:.2f}')
+
+print()
+#------------------------------------------------
