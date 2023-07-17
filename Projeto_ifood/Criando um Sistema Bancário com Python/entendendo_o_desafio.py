46a9ea65a88f70d5dff939b8fa2e62080037190c
Matheus Felipe Braga <111787756+matheusBraga10@users.noreply.github.com>
2023-07-17 16:41:59 -0300

    update - desafio

--- Projeto_ifood/Criando um Sistema Bancário com Python/entendendo_o_desafio.py
+++ Projeto_ifood/Criando um Sistema Bancário com Python/entendendo_o_desafio.py
@@ -9,36 +9,56 @@ devemos umplementar apenas 3 operações:
 - Extrato
 '''
 menu = f'''
-{=*20}
+{'='*20}
 
 [1] - Depositar
 [2] - Sacar
 [3] - Extrato
 [4] - Sair
 
-{=*20}
+{'='*20}
 '''
 saldo = 0
 limite = 500
-extrato = f''''''
 numero_de_saques = 0
 LIMITE_DE_SAQUES = 3
+extrato = f'''[3] - Extrato
 
+Seu saldo atual é de: R$:{saldo:.2f} .
+Seu limite atual é de: R${limite:.2f} .
+Você ainda tem direito de realizar mais {numero_de_saques} saques.'''
 while True:
 
     opcao = input(f'{menu}\n Opção: ')
 
     if opcao == '1':
-        print('[1] - Deposito')
-        
-    
+        print('[1] - Deposito\n')
+        valor = float(input('\nQual o valor deseja depositar na conta? '))
+        if valor > 0:
+            saldo = saldo + valor
+            print(f'Seu depósito de R$:{valor:.2f} foi realizado com sucesso.')
+        else:
+            print('O valor não pode ser efetuado. Por favor, tente novamente.')
+
     elif opcao == '2':
-        print('[2] - Saque')
-        
+        print('[2] - Saque\n')
+        while numero_de_saques < LIMITE_DE_SAQUES and saldo > 0:
+            saque = float(input('Digite o valor que deseja sacar: R$: '))
+            if limite >= saque:
+                numero_de_saques = numero_de_saques + 1
+                limite = limite - saque
+                saldo = saldo - saque
+                print(f'''Saque de R$:{saque:.2f} efetuado com sucesso. Retire seu dinheiro.
+                      Seu saldo foi atualizado para R$:{saldo:.2f} .
+                      ''')
+                break
+            else:
+                print('Você chegou ao limite de saques por dia, ou ultrapassou seu limite de crédito.')
+                break
     elif opcao == '3':
-        print('[3] - Extrato')
+        print(extrato)
         
-    elif opcao == '4'
+    elif opcao == '4':
         print('[4] - Sair')
         break
     else:
