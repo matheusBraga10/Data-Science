82d3e3fa590e1c8ff95ea60d1a3f80e4bbd9ced7
Matheus Felipe Braga <111787756+matheusBraga10@users.noreply.github.com>
2023-07-19 10:54:59 -0300

    args e kwargs

--- Projeto_ifood/Dominando Funções em Python/args_e_kwargs.py
+++ Projeto_ifood/Dominando Funções em Python/args_e_kwargs.py
@@ -4,11 +4,11 @@
 
 def exibir_poema(data_extenso, *tupla, **dicionario): #*args, **kwargs):
     texto = '\n'.join(tupla)
-    meta_dados = '\n'.join([f'{chave.title()}: {valor}' for chave, valor in dicionario.items()]) 
+    meta_dados = '\n'.join([f'{chave.title()}: {valor}'for chave, valor in dicionario.items()]) 
     ''' meta_dados : É uma junção de dicionários descritos abaixo: 
 
         - .join(tupla) : .join faz uma junção das tuplas declaradas na função 'exibir_poema'
-        - for chave, valor in dicionario.items() : 'items' retorna lista de tuplas dentro do 'dicionario' que são 'chave' e 'valor', para o 'for' iterar
+        - for chave, valor in dicionario.items() : 'for' declara 'chave' e 'valor' dentro dos 'items' de 'dicionario'
     '''
     mensagem = f'{data_extenso}\n\n{texto}\n\n{meta_dados}'
     print(mensagem)
