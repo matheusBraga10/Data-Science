31baf7b2c9ae9adb9bfc9a6c1a5ef432491d0f55
Matheus Felipe Braga <111787756+matheusBraga10@users.noreply.github.com>
2023-07-20 14:24:37 -0300

    otimizando...

--- Projeto_ifood/Criando um Sistema Bancário com Python/otimizando_desafio_sistema_bancario.py
+++ Projeto_ifood/Criando um Sistema Bancário com Python/otimizando_desafio_sistema_bancario.py
@@ -1,22 +1,54 @@
 # Desafio - Otimizando o Sistema Bancário com Funções Python
+extrato = ''
 
 def deposito(valor):
     print('[1] - Deposito\n')
-    
+    global extrato
     if valor > 0:
+        saldo = 0
         saldo = saldo + valor
         extrato = extrato + f'Depósito R$: {valor:.2f}\n\n'
         print(f'Seu depósito de R$:{valor:.2f} foi realizado com sucesso.')
     else:
         print('O valor não pode ser efetuado. Por favor, tente novamente.')
 
-def saque():
+def sacar(saque):
+    global extrato
+    print('[2] - Saque\n')
+    LIMITE_DE_SAQUES = 3
+    numero_de_saques = 0
+    while numero_de_saques < LIMITE_DE_SAQUES and saldo > 0:
+        limite = 500
+        if limite >= saque:
+            numero_de_saques = numero_de_saques + 1
+            limite = limite - saque
+            saldo = saldo - saque
+            extrato = extrato + f'Saque R$: {saque:.2f}\nLimite restante R$: {limite:.2f}\nNumero de saques diários restantes:  {LIMITE_DE_SAQUES - numero_de_saques}\nSaldo em conta R$: {saldo:.2f}\n\n'
+            print(f'''Saque de R$:{saque:.2f} efetuado com sucesso. Retire seu dinheiro.
+                    Seu saldo foi atualizado para R$:{saldo:.2f} .
+                    ''')
+            break
+        else:
+            print('Você chegou ao limite de saques por dia, ou ultrapassou seu limite de crédito.')
+            break
+        
+def extrato_conta(deposito , sacar):
+    global extrato
+    global saldo
+    extrato = f'''
 
-def extrato():
+    {'='*20}
 
-def cadastro_usuario():
+    EXTRATO
 
-def cadastro_conta_bancaria():
+    {'='*20}
+    '''
+    extrato = extrato + f'Saldo atual: R$:{saldo:.2f}\n'
+    
+        
+#def cadastro_usuario():
+
+#def cadastro_conta_bancaria():
 
 
 menu = f'''
@@ -25,50 +57,35 @@ menu = f'''
 [1] - Depositar
 [2] - Sacar
 [3] - Extrato
-[4] - Sair
+[4] - Cadastrar usuário
+[5] - Cadastrar conta bancária
+[6] - Sair
 
 {'='*20}
 '''
-saldo = 0
-limite = 500
-numero_de_saques = 0
-LIMITE_DE_SAQUES = 3
-extrato = f'''
 
-{'='*20}
-EXTRATO
-{'='*20}
-
-'''
 while True:
 
     opcao = input(f'{menu}\n Opção: ')
 
     if opcao == '1':
-        valor = float(input('\nQual o valor deseja depositar na conta? '))
+        deposito(float(input('\nQual o valor deseja depositar na conta? ')))
         
     elif opcao == '2':
-        print('[2] - Saque\n')
-        while numero_de_saques < LIMITE_DE_SAQUES and saldo > 0:
-            saque = float(input('Digite o valor que deseja sacar: R$: '))
-            if limite >= saque:
-                numero_de_saques = numero_de_saques + 1
-                limite = limite - saque
-                saldo = saldo - saque
-                extrato = extrato + f'Saque R$: {saque:.2f}\nLimite restante R$: {limite:.2f}\nNumero de saques diários restantes:  {LIMITE_DE_SAQUES - numero_de_saques}\nSaldo em conta R$: {saldo:.2f}\n\n'
-                print(f'''Saque de R$:{saque:.2f} efetuado com sucesso. Retire seu dinheiro.
-                      Seu saldo foi atualizado para R$:{saldo:.2f} .
-                      ''')
-                break
-            else:
-                print('Você chegou ao limite de saques por dia, ou ultrapassou seu limite de crédito.')
-                break
+        sacar(float(input('\nDigite o valor que deseja sacar: R$: ')))
+
     elif opcao == '3':
-        extrato = extrato + f'Saldo atual: R$:{saldo:.2f}\n'
-        print(extrato)
-        
+        extrato_conta(deposito, sacar)
+
     elif opcao == '4':
-        print('[4] - Sair')
+        print(extrato)    
+
+    elif opcao == '5':
+        print(extrato)    
+
+    elif opcao == '6':
+        print('[6] - Sair')
         break
+    
     else:
         print('Opção inválida, tente novamente')
