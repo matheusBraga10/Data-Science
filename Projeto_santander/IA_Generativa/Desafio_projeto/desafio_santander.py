bab33752921931057f3d8fd1f9304adbc12844e4
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2023-09-18 22:24:53 -0300

    18-09

--- Projeto_santander/IA_Generativa/Desafio_projeto/desafio_santander.py
+++ Projeto_santander/IA_Generativa/Desafio_projeto/desafio_santander.py
@@ -1,63 +1,24 @@
 import sqlite3 as conector
+import zipfile 
+from pyspark.sql import SparkSession
+import os
+import findspark
+
+
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
+
+os.environ["SPARK_HOME"] = "/content/spark-3.1.2-bin-hadoop2.7"
+
+
+findspark.init()
 
 try:
-  # abertura da conexão
-  conexao = conector.connect('/home/matheus/Documentos/MeusProjetos/Data-Science/Projeto_santander/IA_Generativa/Desafio_projeto/desafio.db')
-# aquisição de um cursor
-  cursor = conexao.cursor()
+  path = '/home/matheus/Documentos/MeusProjetos/Data-Science/Projeto_santander/IA_Generativa/Desafio_projeto/'
+  spark = SparkSession.buider.master('local[*]').appName('Iniciando com Spark').config('spark.ui.port','4050').getOrCreate()
+  zipfile.Zipfile('/home/matheus/Documentos/MeusProjetos/Data-Science/Projeto_santander/IA_Generativa/Desafio_projeto/archive.zip','r').extractall('/home/matheus/Documentos/MeusProjetos/Data-Science/Projeto_santander/IA_Generativa/Desafio_projeto/')
 
-  comando_01 = '''
-  CREATE TABLE usuario
-  (
-  id INT(11) NOT NULL,
-  nome VARCHAR(30) NOT NULL,
-  PRIMARY KEY (nome)
-  )
-  '''
-  comando_02 = '''
-  CREATE TABLE account
-  (
-  number_account VARCHAR(7) NOT NULL,
-  agency VARCHAR(4) NOT NULL,
-  balance INTEGRER(10),
-  limit_account INTEGRER(10) NOT NULL,
-  FOREIGN KEY (nome) REFERENCES usuario (nome),
-  FOREIGN KEY (number_card) REFERENCES card (number_card),
-  PRIMARY KEY (number_account)
-  );
-  '''
-
-  comando_03 = '''
-  CREATE TABLE card
-  (
-  number_card VARCHAR(20) NOT NULL,
-  limit_card INTEGRER(20) NOT NULL,
-  PRIMARY KEY (number_card)
-  );
-  '''
-  # comando_04 = '''
-  # CREATE TABLE feactures
-  # (
-  # number_card VARCHAR(20) ,
-  # limit_card INTEGRER(20) ,
-  # PRIMARY KEY (number_card)
-  # );
-  # '''
-  # comando_05 = '''
-  # CREATE TABLE news
-  # (
-  # number_card VARCHAR(20) ,
-  # limit_card INTEGRER(20) ,
-  # PRIMARY KEY (number_card)
-  # );
-  # '''
-  # execução do comando: SELECT, INSERT, CREATE...
-  cursor.execute(comando_01)
-  cursor.execute(comando_02)
-  cursor.execute(comando_03)
-
-# Efetivação do comando
-  conexao.commit()
+  
+  
 
 except conector.DatabaseError as erro:
   print('Erro no banco de dados', erro)
